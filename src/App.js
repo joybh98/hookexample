07df3fdabcbe7fdf57b27d5940c3c1ebde8272e5
Joy Bhalla <joybhalla9@gmail.com>
2020-04-04 12:53:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	
+	// Set cartItems with useState so it can use the same state when we are using it below
+	const [cartItems, setCartItems] = useState([])
+	
+	const [scrollDepth, setscrollDepth] = useState([])
+
+	function determineUserScrollDepth() {
+
+		const scrolled = document.document.scrollTop
+		setscrollDepth(scrolled);
+
+	}
+
+	useEffect(() => {
+		window.eventListner('scroll', determineUserScrollDepth)
+	}
+
+	return (
+		<div className="App">	
+					<h1>Items in your cart: {cartItems.toString()}</h1>
+		<button
+				onClick={() => {
+					setCartItems([...cartItems, 'apple'])
+				}}
+		>
+		<p>Add to cart</p>
+
+		</button>
+		</div>
+	);
 }
 
-export default App;
